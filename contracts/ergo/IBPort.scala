57e65d8169fcede740c16f17e1f23403c3a3554d
Mostafa Hosseini <aseyedmostafaho@gmail.com>
2021-06-05 20:45:20 +0430

    code refactored

--- contracts/ergo/IBPort.scala
+++ contracts/ergo/IBPort.scala
@@ -13,30 +13,32 @@
          |  val minValue = 1000000 // TODO: check minValue with node
          |  val check = {
          |    if (INPUTS(0).tokens(1)._1 == linkListNFTToken){ // create Transfer wrap request
-         |       INPUTS(1).tokens(1)._1 == maintainerNFTToken,
+         |      linkListTokenOutput = OUTPUTS(0)
+         |      linkListElementOutput = OUTPUTS(1)
+         |      allOf(Coll(
+         |        INPUTS(1).tokens(1)._1 == maintainerNFTToken,
          |
-         |       linkListTokenOutput = OUTPUTS(0)
-         |       linkListTokenOutput.tokens(0)._1 == linkListTokenRepoId,
-         |       linkListTokenOutput.tokens(0)._2 == INPUTS(0).tokens(0)._2 - 1,
-         |       linkListTokenOutput.tokens(1)._1 == linkListNFTToken,
-         |       linkListTokenOutput.propositionBytes == SELF.propositionBytes,
-         |       linkListTokenOutput.value == INPUTS(0).value - minValue,
+         |        linkListTokenOutput.tokens(0)._1 == linkListTokenRepoId,
+         |        linkListTokenOutput.tokens(0)._2 == INPUTS(0).tokens(0)._2 - 1,
+         |        linkListTokenOutput.tokens(1)._1 == linkListNFTToken,
+         |        linkListTokenOutput.propositionBytes == SELF.propositionBytes,
+         |        linkListTokenOutput.value == INPUTS(0).value - minValue,
          |
-         |       linkListElementOutput = OUTPUTS(1)
-         |       blake2b256(linkListElementOutput.propositionBytes) == linkListElementRepoContractHash,
+         |        blake2b256(linkListElementOutput.propositionBytes) == linkListElementRepoContractHash,
          |
-         |       OUTPUTS(2).tokens(1)._1 == maintainerNFTToken,
+         |        OUTPUTS(2).tokens(1)._1 == maintainerNFTToken
          |     }
-         |    else if (INPUTS(0).tokens(1)._1 == signalTokenNFT  ){ // ChangeStatus
-         |       linkListTokenOutput = OUTPUTS(1)
-         |       linkListTokenOutput.tokens(0)._2 == INPUTS(2).tokens(0)._2 + 1,
-         |       linkListTokenOutput.tokens(0)._1 == linkListTokenRepoId,
-         |       linkListTokenOutput.tokens(1)._1 == linkListNFTToken,
-         |       linkListTokenOutput.propositionBytes == SELF.propositionBytes,
-         |       linkListTokenOutput.value == INPUTS(2).value + minValue,
+         |    else if (INPUTS(0).tokens(1)._1 == signalTokenNFT){ // ChangeStatus
+         |      linkListTokenOutput = OUTPUTS(1)
+         |      allOf(Coll(
+         |        linkListTokenOutput.tokens(0)._2 == INPUTS(2).tokens(0)._2 + 1,
+         |        linkListTokenOutput.tokens(0)._1 == linkListTokenRepoId,
+         |        linkListTokenOutput.tokens(1)._1 == linkListNFTToken,
+         |        linkListTokenOutput.propositionBytes == SELF.propositionBytes,
+         |        linkListTokenOutput.value == INPUTS(2).value + minValue,
          |
-         |       INPUTS(2).propositionBytes == SELF.propositionBytes,
-         |       blake2b256(INPUTS(3).propositionBytes) == linkListElementRepoContractHash,
+         |        INPUTS(2).propositionBytes == SELF.propositionBytes,
+         |        blake2b256(INPUTS(3).propositionBytes) == linkListElementRepoContractHash
          |     }
          |    else false
          |  }
@@ -45,54 +47,61 @@
          |}""".stripMargin
     val maintainerRepo =
       s"""{
+         |val storeInMaintainer: Boolean = {(v: ( (Box, Box), BigInt ) ) => {
+         |    if (v._1._1.tokens.size > 1){
+         |          v._1._2.value == v._1._1.value &&
+         |          v._1._2.tokens(1)._1 == v._1._1.tokens(1)._1 &&
+         |          v._1._2.tokens(1)._2 == v._1._1.tokens(1)._2 + v._2 &&
+         |    }
+         |    else{
+         |          v._1._2.value == b.value + v._2
+         |    }
+         |  }
+         |
+         |val mint: Boolean = {(v: ( (Box, Box) , (Box, BigInt) ) ) => {
+         |  if (v._1._1.tokens.size > 1){
+         |      v._1._2.tokens(1)._1 == v._1._1.tokens(1)._1 &&
+         |      v._1._2.tokens(1)._2 == v._1._1.tokens(1)._2 - v._2._2 &&
+         |      v._1._2.value == b._1.value
+         |    }
+         |    else{
+         |       v._1._2.value == v._1._1.value - v._2._2
+         |       // TODO: get receiver address from signal data, v._2._1
+         |    }
+         |  }
          |val check = {
+         |
          |  if (INPUTS(0).tokens(1)._1 == linkListNFTToken){ // create Transfer wrap request
-         |    INPUTS(0).tokens(0)._1 == linkListTokenRepoId,
-         |    INPUTS(1).propositionBytes == SELF.propositionBytes,
-         |    INPUTS(1).ID == SELF.ID,
          |    linkListTokenOutput = OUTPUTS(0)
          |    linkListElementOutput = OUTPUTS(1)
          |    maintainerOutput = OUTPUTS(2)
-         |
-         |    linkListTokenOutput.tokens(1)._1 == linkListNFTToken
-         |    blake2b256(linkListElementOutput.propositionBytes) == linkListElementRepoContractHash,
-         |    maintainerOutput.propositionBytes == SELF.propositionBytes,
-         |
          |    val amount = linkListElementOutput.R5[BigInt].get
          |
-         |    maintainerOutput.R4[Int].get == INPUTS(0).R4[Int].get,
-         |    if (INPUTS(1).tokens.size > 1){
-         |      maintainerOutput.value == INPUTS(1).value
-         |      maintainerOutput.tokens(1)._1 == INPUTS(1).tokens(1)._1,
-         |      maintainerOutput.tokens(1)._2 == INPUTS(1).tokens(1)._2 + amount,
-         |    }
-         |    else{
-         |      maintainerOutput.value == INPUTS(1).value + amount,
-         |    }
+         |    allOf(Coll(
+         |      INPUTS(0).tokens(0)._1 == linkListTokenRepoId,
+         |      INPUTS(1).propositionBytes == SELF.propositionBytes,
+         |      INPUTS(1).id == SELF.id,
+         |
+         |      linkListTokenOutput.tokens(1)._1 == linkListNFTToken,
+         |      blake2b256(linkListElementOutput.propositionBytes) == linkListElementRepoContractHash,
+         |
+         |      maintainerOutput.propositionBytes == SELF.propositionBytes,
+         |      maintainerOutput.R4[Int].get == INPUTS(0).R4[Int].get,
+         |      storeInMaintainer( ( (INPUTS(1), maintainerOutput), amount) ) == true
          |  }
          |  else if (INPUTS(0).tokens(1)._1 == signalTokenNFT){ // Mint
-         |    blake2b256(INPUTS(0).propositionBytes) == signalRepoContractHash,
-         |    INPUTS(2).propositionBytes == SELF.propositionBytes,
-         |    INPUTS(2).ID == SELF.ID,
-         |
          |    maintainerOutput = OUTPUTS(1)
-         |    maintainerOutput.tokens(0)._1 == maintainerRepoId,
-         |    maintainerOutput.tokens(1)._1 == maintainerNFTToken,
-         |
-         |    OUTPUTS(2).tokens(0)._1 == maintainerRepoId,
-         |
          |    val amount = INPUTS(2).R5[BigInt].get
          |
-         |    if (INPUTS(1).tokens.size > 1){
-         |      maintainerOutput.tokens(1)._1 == INPUTS(2).tokens(1)._1,
-         |      maintainerOutput.tokens(1)._2 == INPUTS(2).tokens(1)._2 - amount,
-         |      maintainerOutput.value == INPUTS(2).value
-         |    }
-         |    else{
-         |       maintainerOutput.value == INPUTS(2).value - amount,
-         |       // TODO: get receiver address from signal data
+         |    allOf(Coll(
+         |      blake2b256(INPUTS(0).propositionBytes) == signalRepoContractHash,
+         |      INPUTS(2).propositionBytes == SELF.propositionBytes,
+         |      INPUTS(2).id == SELF.id,
          |
-         |    }
+         |      maintainerOutput.tokens(0)._1 == maintainerRepoId,
+         |      maintainerOutput.tokens(1)._1 == maintainerNFTToken,
+         |      OUTPUTS(2).tokens(0)._1 == maintainerRepoId,
+         |      mint( (INPUTS(2), maintainerOutput), (OUTPUTS(2), amount) ) == true
          |  }
          |}
          |  sigmaProp (check)
@@ -102,12 +111,12 @@
          |  val minValue = 1000000 // TODO: check minValue with node
          |  val check = {
          |    if (INPUTS(0).tokens(1)._1 == linkListNFTToken){ // create Transfer wrap request
+         |      linkListElementOutput = OUTPUTS(1)
+         |      linkListTokenOutput = OUTPUTS(0)
+         |      allOf(Coll(
          |       INPUTS(1).tokens(1)._1 == maintainerNFTToken,
          |
-         |       linkListTokenOutput = OUTPUTS(0),
          |       linkListTokenOutput.tokens(1)._1 == linkListNFTToken,
-         |
-         |       linkListElementOutput = OUTPUTS(1),
          |       linkListElementOutput.propositionBytes == SELF.propositionBytes,
          |       linkListElementOutput.tokens(0)._1 == linkListTokenRepoId,
          |       linkListElementOutput.tokens(0)._2 == 1,
@@ -115,15 +124,19 @@
          |       linkListElementOutput.R5[BigInt].isDefined, // request amount
          |       linkListElementOutput.value == minValue,
          |
-         |       OUTPUTS(2).tokens(1)._1 == maintainerNFTToken,
+         |       OUTPUTS(2).tokens(1)._1 == maintainerNFTToken
+         |      ))
          |     }
          |    else if (INPUTS(0).tokens(1)._1 == signalTokenNFT){ // approve
-         |       INPUTS(2).tokens(1)._1 == linkListNFTToken,
-         |       INPUTS(3).propositionBytes == SELF.propositionBytes,
-         |       INPUTS(3).ID == SELF.ID,
+         |      allOf(Coll(
+         |        INPUTS(2).tokens(1)._1 == linkListNFTToken,
+         |        INPUTS(3).propositionBytes == SELF.propositionBytes,
+         |        INPUTS(3).id == SELF.id
+         |      ))
          |     }
          |    else false
          |  }
          |
          |  sigmaProp (check)
          |}""".stripMargin
+         
